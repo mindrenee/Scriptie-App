7d69a7afb9b1e4924dd94357b7468e27f448b9af
mindrenee <reneewitsenburg@live.nl>
2015-07-19 13:46:20 +0200

    social media add

--- js/social.js
+++ js/social.js
@@ -1,5 +1,4 @@
 function share() {
 	var message = document.forms["social"]["bericht"].value;
-	//alert(message);
-	window.plugins.socialsharing.share(message);
+	share(message, null, null, null);
 }
